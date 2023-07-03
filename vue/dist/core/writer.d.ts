d4f0c3071678251f600c7d0c1b508f52cf52ec70
David Blanchard <dpjb@hotmail.fr>
2023-07-03 18:19:55 +0200

    Remove dist files

--- vue/dist/core/writer.d.ts
+++
@@ -1,6 +0,0 @@
-export class Writer {
-    constructor(ownerDocument: any, source: any, speed: any, makeTypos: any, finishedCallback: any);
-    writeLikeAHuman(toWrite: any, toPlace: any): Promise<void>;
-    finishedEvent(html: any): void;
-    #private;
-}
